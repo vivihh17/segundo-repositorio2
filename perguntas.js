2c482afae5f6665344a57960a1672d5f055705f5
bolsonarokun <nascimento.alves.ana@escola.pr.gov.br>
2025-05-28 22:29:36 -0300

    Aula 28/05 3.0

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
-criarCartao(
+criaCartao(
     'Progamação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
-criarCartao(
+criaCartao(
     'Geografia',
     'Qual a Capital da França?',
     'A Capital da França é Paris'
 )
 
-criarCartao(
+criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
-criarCartao(
+criaCartao(
     'Programação',
     'O que é JavaScript?',
     'JavaScript é uma linguagem de programação'
